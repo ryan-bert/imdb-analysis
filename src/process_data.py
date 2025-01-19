46ab07ecd96df263f86b7f169bbcb9332e39665f
ryan-bert <ryanberti66@gmail.com>
2025-01-19 20:24:09 +0200

    explode() works

--- src/process_data.py
+++ src/process_data.py
@@ -5,7 +5,7 @@ CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
 DATA_DIR = os.path.join(CURRENT_DIR, '../data')
 
 def main():
-    
+
     # Load ratings
     ratings_df = pd.read_csv(os.path.join(DATA_DIR, 'my_ratings_with_cast.csv'))
 
@@ -18,12 +18,16 @@ def main():
     ratings_df.rename(columns={'Directors': 'Director'}, inplace=True)
 
     # Convert to long-form data
-    ratings_df = ratings_df['Cast'].str.split(', ')
-    ratings_df = ratings_df.explode('Cast')
+    ratings_df['Cast'] = ratings_df['Cast'].str.split(', ')
+    ratings_df = ratings_df.explode('Cast').reset_index(drop=True)
 
-    print(ratings_df)
+    # Rename and reorder columns to make Actor the first column
+    ratings_df.rename(columns={'Cast': 'Actor'}, inplace=True)
+    columns_order = ['Actor'] + [col for col in ratings_df.columns if col != 'Actor']
+    ratings_df = ratings_df[columns_order]
 
-     
+    # Save the updated DataFrame back to a CSV
+    updated_csv_path = os.path.join(DATA_DIR, 'actors_long.csv')
 
 
 if __name__ == '__main__':
