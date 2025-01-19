49ed93965cd771f32e9e206356b93087ab7e7c03
ryan-bert <ryanberti66@gmail.com>
2025-01-19 20:55:40 +0200

    Got favorite actors

--- src/analysis.py
+++ src/analysis.py
@@ -17,11 +17,19 @@ def main():
         My_Avg_Rating=('Your Rating', 'mean')
     ).reset_index()
     # Sort by Appearances and then by Avg_IMDb_Rating
-    actor_summary = actor_summary.sort_values(by=['Appearances', 'Avg_IMDb_Rating'], ascending=[False, False])
+    actor_summary = actor_summary.sort_values(by=['Appearances', 'My_Avg_Rating'], ascending=[False, False])
 
-    # Save the updated DataFrame back to a CSV
+    # Rename columns
+    actor_summary.rename(columns={'Avg_IMDb_Rating': 'Avg IMDb Rating', 'My_Avg_Rating': 'My Avg Rating'}, inplace=True)
+
+    # Save the actor summary back to a CSV
     summary_csv_path = os.path.join(OUTPUT_DIR, 'actor_summary.csv')
     actor_summary.to_csv(summary_csv_path, index=False)
+
+    # Get my favorite actors
+    favorite_actors = actor_summary[(actor_summary['My Avg Rating'] >= 8.0) & (actor_summary['Appearances'] >= 5)]
+    favorite_actors = favorite_actors.sort_values(by='My Avg Rating', ascending=False)
+    print(favorite_actors)
 
 
 if __name__ == '__main__':
